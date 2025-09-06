954072e1bcc96598bf9eae57ab06f08e6da875c3
Linga Annela <lingareddy.annela@gmail.com>
2025-09-06 08:38:52 -0400

    NXT-100: Py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow frontend domain
+origins = [
+    "https://nextgenerationmart.com",  # your Angular app
+    "http://localhost:4200"            # optional, for local dev
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
